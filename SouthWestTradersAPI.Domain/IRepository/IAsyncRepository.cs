8d44e6ba7dbae50395e5aad9d8ab7e1259da77db
Khotso Bore <kbore@jhb.dvt.co.za>
2022-04-22 21:49:43 +0200

    async and base repository added

---
+++ SouthWestTradersAPI.Domain/IRepository/IAsyncRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SouthWestTradersAPI.Domain.IRepository
+{
+    public interface IAsyncRepository<TEntity> where TEntity : class
+    {
+        Task<List<TEntity>> ListAsync(Expression<Func<TEntity,bool>> expression);
+
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression);
+
+        Task RemoveAsync(Expression<Func<TEntity, bool>> expression);
+
+        Task<TEntity> AddAsync(TEntity entity);
+
+        Task<TEntity> UpdateAsync(Expression<Func<TEntity, bool>> expression, TEntity entity);
+
+
+    }
+}
---
+++ SouthWestTradersAPI.Infrastructure/Repository/BaseRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SouthWestTradersAPI.Domain.IRepository;
+using SouthWestTradersAPI.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SouthWestTradersAPI.Infrastructure.Repository
+{
+    public class BaseRepository<TEntity> : IAsyncRepository<TEntity> where TEntity : class
+    {
+        private readonly SouthWestTradersDBContext context;
+        private readonly DbSet<TEntity> dbset;
+
+        public BaseRepository(SouthWestTradersDBContext context)
+        {
+            this.context = context;
+            dbset = context.Set<TEntity>();
+        }
+        
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+           await dbset.AddAsync(entity);
+           context.SaveChanges();
+           return entity;
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            return await dbset.Where(expression).FirstAsync();
+        }
+
+        public async Task<List<TEntity>> ListAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            return await dbset.Where(expression).ToListAsync();
+        }
+
+        public async Task RemoveAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            var response = await dbset.Where(expression).FirstAsync();
+            if (response == null)
+                throw new Exception($"{typeof(TEntity).FullName} not found");
+
+            dbset.Remove(response);
+            context.SaveChanges();
+        }
+
+        public async Task<TEntity> UpdateAsync(Expression<Func<TEntity, bool>> expression, TEntity entity)
+        {
+            var response = await dbset.Where(expression).FirstAsync();
+            if (response == null)
+                throw new Exception($"{typeof(TEntity).FullName} not found");
+
+            dbset.Update(entity);
+            context.SaveChanges();
+            return entity;
+
+        }
+    }
+}
